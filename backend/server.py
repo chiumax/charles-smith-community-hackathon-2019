4eea6872ca3fc52c2f1f724624c2dbaadb9e509d
Christopher Stallard <chrisstallard4@gmail.com>
2019-01-27 17:37:00 -0500

    neel

--- backend/server.py
+++ backend/server.py
@@ -11,7 +11,7 @@ def homepage():
 def book():
     return book
 @app.route("/flights")
-def list_flights():
+def list_airports():
     ret = {"dep":[],"arr":[]}
     d = request.args["lAd"] + ", "+request.args["lCi"]+request.args["lSt"]+request.args["lCo"]
     d_loc = latlong_utils.geocode(d)
@@ -19,13 +19,18 @@ def list_flights():
     l = air.getAirports()
     for i in l:
         ret["dep"].append({i[0]:i[1]})
-    a = request.args["dCi"]+request.args["dSt"]+request.args["dCo"]
+    a = request.args["dAd"]+","+request.args["dCi"]+request.args["dSt"]+request.args["dCo"]
     a_loc = latlong_utils.geocode(a)
     air2 = back.airport(a_loc["lat"], a_loc["lng"])
     l = air2.getAirports()
     for i in l:
         ret["arr"].append({i[0]:i[1]})
     return ret
+@app.route("/find_flights")
+def find_flights():
+    ret={}
+    d = request.args["depart"]
+    a = request.args["arrive"]
 @app.route("/hotels")
 def list_hotels():
     lat = request.args["lat"]
