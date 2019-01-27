0262d650bbc63dbefd836b0515a268cbf7a73d25
frenchfries11234 <45515297+frenchfries11234@users.noreply.github.com>
2019-01-27 16:02:42 -0500

    Add files via upload
    nels

--- backend/back.py
+++ backend/back.py
@@ -14,7 +14,7 @@ class planes:
         self.date=date
         
     def getPlanes(self):
-        #returns price, class, departure1, arrival 1, derpature2(if applicable), and arrival2
+        #returns [ full list of [price, class, departure1 info, arrival 1 info, derpature2(if applicable) info, and arrival2 info]]
         try:
             response = amadeus.shopping.flight_offers.get(origin=self.origin, destination=self.destination, departureDate=self.date).data
             final=[]
@@ -35,23 +35,42 @@ class planes:
         a.sort(key=lambda x: x[0])
         return a
 
+class airports:
+    def __init__(self,lat,long):
+        #takes lat and long
+        self.long=long
+        self.lat=lat
+
+    def getAirports(self):
+        #returns [3 long list of [code, name, lat, long]]
+        new=[]
+        a=amadeus.reference_data.locations.airports.get(longitude=self.long, latitude=self.lat).data
+        for i in range(3):
+            new.append([a[i]['iataCode'], a[i]['detailedName'], a[i]['geoCode']['latitude'],a[i]['geoCode']['longitude']])
+        return new
+    
+
 class hotels:
     def __init__(self,dest):
-        #takes city name(3 letters)
+        #takes city code(3 letters)
         self.dest=dest
 
     def getHotels(self):
-        #returns price, name, currency, longitude, lat, and hotel description
+        #returns [full list of [price, name, currency, longitude, lat, hotel description]]
         new=[]
         for i in amadeus.shopping.hotel_offers.get(cityCode = self.dest).data:
-            new.append([float(i['offers'][0]['price']['total']),i['offers'][0]['price']['currency'],i['hotel']['name'],i['hotel']['longitude'],i['hotel']['latitude'],i['offers'][0]['room']['description']])
+            new.append([float(i['offers'][0]['price']['total']),i['offers'][0]['price']['currency'],i['hotel']['name'],i['hotel']['latitude'],i['hotel']['longitude'],i['offers'][0]['room']['description']])
         return new
 
     def getHotelsSorted(self):
         a=self.getHotels()
         a.sort(key=lambda x: x[0])
         return a
+
 '''
+airport=airports(39.122379,-77.245842)
+print(airport.getAirports())
+
 hotel=hotels('TLV')
 for i in hotel.getHotelsSorted():
     print(i)
